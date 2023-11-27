ac77c8a87b0e90a25761ad1bb7ba9128fb7ec5e7
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2023-11-27 03:39:53 +0000

    Add day 1 solution (2022)

---
+++ 2022/python/day01.py
@@ -0,0 +1,32 @@
+
+"""
+Day 1 - Calorie Counting
+"""
+
+def parse_input() -> list[list[int]]:
+    with open('data/day01.txt', encoding='utf8') as f:
+        data = str(f.read())
+
+    return [
+        [int(meal) for meal in elf.split()]
+        for elf in data.split('\n\n')
+    ]
+
+
+def part1(elves: list[list[int]]) -> int:
+    return sum(max(elves, key=sum))
+
+def part2(elves: list[list[int]]) -> int:
+    total_calories = sorted((sum(elf) for elf in elves), reverse=True)
+    return sum(total_calories[:3])
+
+
+def main():
+    elves = parse_input()
+
+    print(part1(elves))
+    print(part2(elves))
+
+
+if __name__ == '__main__':
+    main()
